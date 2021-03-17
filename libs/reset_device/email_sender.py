8169318fa9372412c8bdbd173157e88f85f81102
aplus808 <aplus808@gmail.com>
2021-03-17 12:04:12 -0700

    added config_file_hash()

--- libs/reset_device/email_sender.py
+++ libs/reset_device/email_sender.py
@@ -1,14 +1,24 @@
 import socket
 import subprocess
 import os
-from reset_lib import config_file_hash
 
-ip_addr = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+def config_file_hash():
+	config_file = open('/etc/raspiwifi/raspiwifi.conf')
+	config_hash = {}
+
+	for line in config_file:
+		line_key = line.split("=")[0]
+		line_value = line.split("=")[1].rstrip()
+		config_hash[line_key] = line_value
+
+	return config_hash
+
 config_hash = config_file_hash()
 recipient = config_hash["email_address"]
 
 def send_ip_message():
     if os.path.isfile('/etc/raspiwifi/client_first_boot'):
+        ip_addr = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
         body = 'Raspberry Pi IP: ' + ip_addr
         subject = 'Pi-hole ready!'
         cmd = 'echo "' + body + '" | mail -s "' + subject + '" ' + recipient
