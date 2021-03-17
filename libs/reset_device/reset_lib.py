1d7b197d917e3e0907de4a244c8f85896467ab74
aplus808 <aplus808@gmail.com>
2021-03-17 12:04:40 -0700

    migrated from os.system() to subprocess.run()

--- libs/reset_device/reset_lib.py
+++ libs/reset_device/reset_lib.py
@@ -25,7 +25,7 @@ def wpa_check_activate(wpa_enabled, wpa_key):
 
 	if wpa_enabled == '1' and wpa_active == False:
 		reboot_required = True
-		os.system('cp /usr/lib/raspiwifi/reset_device/static_files/hostapd.conf.wpa /etc/hostapd/hostapd.conf')
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/hostapd.conf.wpa", "/etc/hostapd/hostapd.conf"])
 
 	if wpa_enabled == '1':
 		with fileinput.FileInput('/etc/hostapd/hostapd.conf', inplace=True) as hostapd_conf:
@@ -33,7 +33,7 @@ def wpa_check_activate(wpa_enabled, wpa_key):
 				if 'wpa_passphrase' in line:
 					if 'wpa_passphrase=' + wpa_key not in line:
 						print('wpa_passphrase=' + wpa_key)
-						os.system('reboot')
+						subprocess.run("reboot")
 					else:
 						print(line, end = '')
 				else:
@@ -41,7 +41,7 @@ def wpa_check_activate(wpa_enabled, wpa_key):
 
 	if wpa_enabled == '0' and wpa_active == True:
 		reboot_required = True
-		os.system('cp /usr/lib/raspiwifi/reset_device/static_files/hostapd.conf.nowpa /etc/hostapd/hostapd.conf')
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/hostapd.conf.nowpa", "/etc/hostapd/hostapd.conf"])
 
 	return reboot_required
 
@@ -81,17 +81,17 @@ def reset_to_host_mode():
 	if not os.path.isfile('/etc/raspiwifi/host_mode'):
 		email_sender.send_reset_message()
 		subprocess.run(["aplay", "/usr/lib/raspiwifi/reset_device/button_chime.wav"])
-		subprocess.run("rm", "-f", "/etc/wpa_supplicant/wpa_supplicant.conf")
-		subprocess.run("rm", "-f", "/home/pi/Projects/RaspiWifi/tmp/*")
-		subprocess.run("rm", "/etc/cron.raspiwifi/apclient_bootstrapper")
-		subprocess.run("cp", "/usr/lib/raspiwifi/reset_device/static_files/aphost_bootstrapper", "/etc/cron.raspiwifi/")
-		subprocess.run("chmod", "+x", "/etc/cron.raspiwifi/aphost_bootstrapper")
-		subprocess.run("mv", "/etc/dhcpcd.conf", "/etc/dhcpcd.conf.original")
-		subprocess.run("cp", "/usr/lib/raspiwifi/reset_device/static_files/dhcpcd.conf", "/etc/")
-		subprocess.run("mv", "/etc/dnsmasq.conf", "/etc/dnsmasq.conf.original")
-		subprocess.run("cp", "/usr/lib/raspiwifi/reset_device/static_files/dnsmasq.conf", "/etc/")
-		subprocess.run("cp", "/usr/lib/raspiwifi/reset_device/static_files/dhcpcd.conf", "/etc/")
-		subprocess.run("pihole", "disable")
-		subprocess.run("rm", "-f", "/etc/systemd/system/dnsmasq.service")
-		subprocess.run("touch", "/etc/raspiwifi/host_mode")
-		os.system('reboot')
+		subprocess.run(["rm", "-f", "/etc/wpa_supplicant/wpa_supplicant.conf"])
+		subprocess.run(["rm", "-f", "/home/pi/Projects/RaspiWifi/tmp/*"])
+		subprocess.run(["rm", "/etc/cron.raspiwifi/apclient_bootstrapper"])
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/aphost_bootstrapper", "/etc/cron.raspiwifi/"])
+		subprocess.run(["chmod", "+x", "/etc/cron.raspiwifi/aphost_bootstrapper"])
+		subprocess.run(["mv", "/etc/dhcpcd.conf", "/etc/dhcpcd.conf.original"])
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/dhcpcd.conf", "/etc/"])
+		subprocess.run(["mv", "/etc/dnsmasq.conf", "/etc/dnsmasq.conf.original"])
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/dnsmasq.conf", "/etc/"])
+		subprocess.run(["cp", "/usr/lib/raspiwifi/reset_device/static_files/dhcpcd.conf", "/etc/"])
+		subprocess.run(["pihole", "disable"])
+		subprocess.run(["rm", "-f", "/etc/systemd/system/dnsmasq.service"])
+		subprocess.run(["touch", "/etc/raspiwifi/host_mode"])
+		subprocess.run("reboot")
